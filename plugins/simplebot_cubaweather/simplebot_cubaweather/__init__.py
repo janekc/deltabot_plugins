79c78a9b0e34a5d909946dd1ad51e13135627183
adbenitez <asieldbenitez@gmail.com>
2020-02-16 22:38:17 -0500

    some tweaks to simplebot_cubaweather

--- plugins/simplebot_cubaweather/simplebot_cubaweather/__init__.py
+++ plugins/simplebot_cubaweather/simplebot_cubaweather/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-import gettext
-import os
-
 from simplebot import Plugin, PluginCommand
 from cuba_weather import RCApiClient
 
@@ -10,29 +7,23 @@ class CubaWeather(Plugin):
 
     name = 'CubaWeather'
     version = '0.1.0'
+    description = 'Buscar información del clima usando redcuba.cu'
 
     @classmethod
     def activate(cls, bot):
         super().activate(bot)
 
-        localedir = os.path.join(os.path.dirname(__file__), 'locale')
-        lang = gettext.translation('simplebot_cubaweather', localedir=localedir,
-                                   languages=[bot.locale], fallback=True)
-        lang.install()
-        cls.description = _('Search weather info using cuba-weather from redcuba.cu')
         cls.commands = [
-            PluginCommand('/cuwtr', ['[text]'], _('Search weather info using cuba-weather from redcuba.cu'), cls.cuwtr_cmd)]
+            PluginCommand('/cuwtr', ['<lugar>'], 'Buscar información del clima para el lugar dado', cls.cuwtr_cmd)]
         cls.bot.add_commands(cls.commands)
 
     @classmethod
     def cuwtr_cmd(cls, ctx):
         chat = cls.bot.get_chat(ctx.msg)
 
-        if not ctx.text:
-            chat.send_text('Send me a cuban location ej: /cuwtr Santiago')
-        else:
+        if ctx.text:
             api = RCApiClient()
-
             weather = api.get(ctx.text, suggestion=True)
-
             chat.send_text(str(weather))
+        else:
+            chat.send_text('Enviame un lugar de Cuba, ej: /cuwtr Santiago')
