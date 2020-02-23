240c4e90414d3d07b6af8e26b08535b71e40a0cb
adbenitez <asieldbenitez@gmail.com>
2020-02-23 16:30:06 -0500

    fix assert

--- plugins/simplebot_reversi/simplebot_reversi/reversi.py
+++ plugins/simplebot_reversi/simplebot_reversi/reversi.py
@@ -57,7 +57,7 @@ class Board:
     def move(self, coord):
         y, x = sorted(coord.lower())
         x = 'abcdefgh'.find(y)
-        assert x >= 0, 'Invalid move'
+        assert x <= 0, 'Invalid move'
         y = int(x) - 1
 
         flipped = self.get_flipped(self.turn, x, y)
