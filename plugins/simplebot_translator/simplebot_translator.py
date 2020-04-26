7858e9ce2c762902698981de1381b7f8e595e743
adbenitez <asieldbenitez@gmail.com>
2020-04-25 21:00:47 -0400

    port translator plugin to new API

---
+++ plugins/simplebot_translator/simplebot_translator.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from deltabot.hookspec import deltabot_hookimpl
+from translators.google import LANGUAGES
+import translators as ts
+
+
+version = '1.0.0'
+LANGUAGES = '\n'.join(
+    ['* {}: {}'.format(v, k)
+     for k, v in sorted(LANGUAGES.items(), key=lambda e: e[1])])
+
+
+@deltabot_hookimpl
+def deltabot_init(bot):
+    bot.commands.register(name="/tr", func=process_tr_cmd)
+
+
+def process_tr_cmd(cmd):
+    """Translate text.
+
+    You need to pass origin and destination language. If you send the
+    command along you will get the list of supported languages and their
+    code.
+    Example: `/tr en es hello world`
+    """
+    if cmd.payload:
+        l1, l2, text = cmd.payload.split(maxsplit=2)
+        return ts.google(text=text, from_language=l1, to_language=l2,
+                         host='https://translate.google.com')
+    else:
+        return LANGUAGES
