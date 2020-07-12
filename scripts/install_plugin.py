a5be753b9db7c47b1a42ad0f483d9375c0afe9fe
adbenitez <asieldbenitez@gmail.com>
2020-07-12 14:30:02 -0400

    update install_plugin.py

--- scripts/install_plugin.py
+++ scripts/install_plugin.py
@@ -10,11 +10,11 @@ if __name__ == '__main__':
     for i, p in enumerate(plugins):
         print('{}. {}'.format(i+1, p))
     print('{}. INSTALL ALL'.format(pcount+1))
-    i = int(input('* Select plugin: ')) - 1
+    to_install = [int(p) - 1 for p in input('* Select plugins: ').split()]
 
-    if i != pcount:
-        plugins = [plugins[i]]
+    if to_install[0] != pcount:
+        plugins = [plugins[i] for i in to_install]
 
     cmd = 'pip install -U "{}"'
     for p in plugins:
-        os.system(cmd.format(os.path.join(pdir, p)))
+        print(cmd.format(os.path.join(pdir, p)))
