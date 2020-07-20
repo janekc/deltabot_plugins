a23a2d774c78b8c1c349d302af981fda3020148c
adbenitez <asieldbenitez@gmail.com>
2020-07-20 02:14:43 -0400

    wikiquote: add locale support

--- plugins/simplebot_wikiquote/simplebot_wikiquote/__init__.py
+++ plugins/simplebot_wikiquote/simplebot_wikiquote/__init__.py
@@ -12,10 +12,14 @@ from deltabot.commands import IncomingCommand
 version = '1.0.0'
 
 
+# ======== Hooks ===============
+
 @deltabot_hookimpl
 def deltabot_init(bot: DeltaBot) -> None:
     bot.commands.register(name="/quote", func=cmd_quote)
 
+
+# ======== Commands ===============
 
 def cmd_quote(command: IncomingCommand, replies: Replies) -> None:
     """Get Wikiquote quotes.
@@ -23,18 +27,29 @@ def cmd_quote(command: IncomingCommand, replies: Replies) -> None:
     Search in Wikiquote or get the quote of the day if no text is given.
     Example: `/quote Richard Stallman`
     """
+    locale = get_locale(command.message.get_sender_contact().addr)
+    if locale in wq.supported_languages():
+        lang = locale
+    else:
+        lang = None
     if command.payload:
-        authors = wq.search(command.payload)
+        authors = wq.search(command.payload, lang=lang)
         if authors:
             if command.payload.lower() == authors[0].lower():
                 author = authors[0]
             else:
                 author = choice(authors)
             quote = '"{}"\n\n― {}'.format(
-                choice(wq.quotes(author, max_quotes=200)), author)
+                choice(wq.quotes(author, max_quotes=200, lang=lang)), author)
         else:
             quote = 'No quote found for: {}'.format(command.payload)
     else:
-        quote = '"{}"\n\n― {}'.format(*wq.quote_of_the_day())
+        quote = '"{}"\n\n― {}'.format(*wq.quote_of_the_day(lang=lang))
 
     replies.add(text=quote)
+
+
+# ======== Utilities ===============
+
+def get_locale(addr: str) -> str:
+    return dbot.get('locale', scope=addr) or dbot.get('locale') or 'en'
