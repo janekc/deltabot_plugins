a496ba2cfc2b0399a2a85c3cdd821264776ad1c6
adbenitez <asieldbenitez@gmail.com>
2020-10-11 00:41:51 -0400

    friends: add /friends_profile command, improve /friends_list

--- plugins/simplebot_friends/simplebot_friends/__init__.py
+++ plugins/simplebot_friends/simplebot_friends/__init__.py
@@ -22,12 +22,12 @@ def deltabot_init(bot: DeltaBot) -> None:
     dbot = bot
     db = get_db(bot)
 
-    getdefault('max_bio_len', '500')
+    getdefault('max_bio_len', '1000')
 
     dbot.commands.register('/friends_join', cmd_join)
     dbot.commands.register('/friends_leave', cmd_leave)
     dbot.commands.register('/friends_list', cmd_list)
-    dbot.commands.register('/friends_me', cmd_me)
+    dbot.commands.register('/friends_profile', cmd_profile)
 
 
 # ======== Commands ===============
@@ -67,20 +67,34 @@ def cmd_leave(command: IncomingCommand, replies: Replies) -> None:
 def cmd_list(command: IncomingCommand, replies: Replies) -> None:
     """Get the list of users and their biography.
     """
-    users = ['{}:\n{}'.format(f['addr'], f['bio']) for f in db.get_users()]
-    text = '\n\n―――――――――――――――\n\n'.join(users) or 'Empty List'
-    replies.add(text=text)
-
-
-def cmd_me(command: IncomingCommand, replies: Replies) -> None:
-    """See your biography.
-    """
-    addr = command.message.get_sender_contact().addr
-    bio = db.get_bio(addr)
-    if bio is None:
-        replies.add(text='You have not set a biography yet')
+    users = []
+    for row in db.get_users():
+        contact = command.bot.get_contact(row['addr'])
+        users.append('{}:\n{}... /friends_profile_{}'.format(
+            row['addr'], row['bio'][:100], contact.id))
+    if users:
+        while users:
+            replies.add(text='\n\n―――――――――――――――\n\n'.join(users[:50]))
+            users = users[50:]
     else:
-        replies.add(text='{}:\n{}'.format(addr, bio))
+        replies.add(text='Empty List')
+
+
+def cmd_profile(command: IncomingCommand, replies: Replies) -> None:
+    """See the biography of the given address or your own in no address provided.
+    """
+    if command.payload.isnumeric():
+        contact = command.bot.get_contact(int(command.payload))
+    elif '@' not in command.payload:
+        contact = command.message.get_sender_contact()
+    else:
+        contact = command.bot.get_contact(command.payload)
+    bio = db.get_bio(contact.addr)
+    if bio is None:
+        replies.add(text='No biography found for {}'.format(contact.addr))
+    else:
+        replies.add(filename=contact.get_profile_image(),
+                    text='{}:\n{}'.format(command.payload, bio))
 
 
 # ======== Utilities ===============
