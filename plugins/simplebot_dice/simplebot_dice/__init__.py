0a707c652f87d21117ac64cecaaa2351dbcbf5fd
adbenitez <asieldbenitez@gmail.com>
2021-01-31 16:02:36 -0500

    simplebot_dice: use quote reply

--- plugins/simplebot_dice/simplebot_dice/__init__.py
+++ plugins/simplebot_dice/simplebot_dice/__init__.py
@@ -21,29 +21,27 @@ def deltabot_init(bot: DeltaBot) -> None:
 def cmd_dice(command: IncomingCommand, replies: Replies) -> None:
     """Roll a dice.
     """
-    _roll_dice(int(command.payload or 1), command)
+    _roll_dice(int(command.payload or 1), command.message, replies)
 
 
 def cmd_dice2(command: IncomingCommand, replies: Replies) -> None:
     """Roll two dices.
     """
-    _roll_dice(2, command)
+    _roll_dice(2, command.message, replies)
 
 
 def cmd_dice5(command: IncomingCommand, replies: Replies) -> None:
     """Roll five dices.
     """
-    _roll_dice(5, command)
+    _roll_dice(5, command.message, replies)
 
 
-def _roll_dice(count: int, command: IncomingCommand) -> None:
+def _roll_dice(count: int, quote: Message, replies: Replies) -> None:
     dices = []
     total = 0
     for i in range(count):
         rand = random.randrange(0, 6)
         total += rand + 1
         dices.append(DICES[rand])
-    msg = Message.new_empty(command.bot.account, 'text')
-    msg.quote = command.message
-    msg.set_text('{} ({})'.format(' '.join(dices), total))
-    command.message.chat.send_msg(msg)
+    replies.add(
+        text='{} ({})'.format(' '.join(dices), total), quote=quote)
