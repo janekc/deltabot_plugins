dc7ee57e099ceda8dfb2f600ef2fd27bd5e251bf
Janek <janek@merlinux.eu>
2021-02-05 12:41:35 +0100

    date

--- plugins/deltabot_activeusers/deltabot_activeusers/__init__.py
+++ plugins/deltabot_activeusers/deltabot_activeusers/__init__.py
@@ -60,7 +60,7 @@ def cmd_info(command: IncomingCommand, replies: Replies) -> None:
     """Shows info
     """
     if check_priv(command.message, dbot):
-        replies.add(text='💻 Userbot active on: {} '.format(socket.gethostname()))
+        replies.add(text='?? Userbot active on: {} '.format(socket.gethostname()))
         replies.add(text='Available commands:\n/info - show this info \n/refresh - scan logs \n/show <all|active|inactive> <hours default=24>\nshow active or inactive users in the last n hours')
 
 
@@ -75,36 +75,35 @@ def cmd_refresh(command: IncomingCommand, replies: Replies) -> None:
 def cmd_show(command: IncomingCommand, replies: Replies) -> None:
     """Shows last login dates for every user seen
     Show active or inactive users in the last n hours
-
-    Todo: use cmd handling
     """
     if check_priv(command.message, dbot):
         usercount = 0
         textlist = "❌ Wrong syntax!\n/show <all|active|inactive> <hours default=24>"
         text = command.payload
         args = command.payload.split(maxsplit=1)
-        subcommand = args[0]
-        parameter = args[1] if len(args) == 2 else ''
-        startdate = datetime.now(timezone(timedelta(hours=1))) - timedelta(hours=24)
-        if parameter:
-            try:
-                parameter = int(parameter)
-            except ValueError as e:
-                replies.add(text="❌ Wrong Syntax!\nParameter must be a number\n/show <all|active|inactive> <hours default=24>")
-                return
-            startdate = datetime.now(timezone(timedelta(hours=1))) - timedelta(hours=parameter)
-        if subcommand == "all":
-            textlist = ""
-            for user, timestamp in db.deltabot_list_users():
-                usercount = usercount + 1
-                textlist = textlist + "{0:25} {1} \n".format(user, timestamp[:-13])
-            textlist = textlist + "\n\nUsers: {}".format(usercount)
-        if subcommand == "active":
-            textlist = "Showing users who have been seen since {}\n\n".format(startdate)
-            textlist = textlist + comparedatetime(1, startdate)
-        if subcommand == "inactive":
-            textlist = "Showing users who have NOT been seen since {}\n\n".format(startdate)
-            textlist = textlist + comparedatetime(0, startdate)
+        if len(args) == 1:
+            subcommand = args[0]
+            parameter = args[1] if len(args) == 2 else ''
+            now = datetime.now()
+            startdate = now - timedelta(hours=24)
+            outfile = ""
+            if parameter:
+                try:
+                    parameter = int(parameter)
+                except ValueError as e:
+                    replies.add(text="❌ Wrong Syntax!\nParameter must be a number\n/show <all|active|inactive> <hours default=24>")
+                    return
+                startdate = now - timedelta(hours=parameter)
+            if subcommand == "all":
+                usercount, outfile = writetofile(2, startdate, now)
+                textlist = "Sending a List of all users\n Users counted: {}".format(usercount)
+            if subcommand == "active":
+                usercount, outfile = writetofile(1, startdate, now)
+                textlist = "Sending a List of users who have been seen since {} \n Users counted: {}".format(startdate, usercount)
+            if subcommand == "inactive":
+                usercount, outfile = writetofile(0, startdate, now)
+                textlist = "Sending a List of users who have NOT been seen since {}\n Users counted: {}".format(startdate,  usercount)
+            replies.add(filename=outfile)
         replies.add(text=textlist)
         
 
@@ -128,27 +127,11 @@ def parse(file):
     with open(file, "r") as logfile:
         for line in logfile:
             matchLogin = re.search(r'Login: user=<([a-zA-Z0-9_.+-]+@testrun.org)', line)
-            if matchLogin:
+            if matchLogin: 
                 matchDate = re.match(r'\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d.\d\d\d\d\d\d\+\d\d:00', line)
-                if matchDate:
+                if matchDate: 
                     lastseen.update({matchLogin.group()[13:]: matchDate.group()})
     return lastseen
-
-
-def comparedatetime(sign, startdate):
-    """write to file
-    """
-    textlist = ""
-    usercount = 0
-    for user, timestamp in db.list_mailusers():
-        if sign == 1 and startdate < datetime.fromisoformat(timestamp):
-            usercount = usercount + 1
-            textlist = textlist + "{0:25} {1} \n".format(user, timestamp[:-13])
-        if sign == 0 and startdate > datetime.fromisoformat(timestamp):
-                usercount = usercount + 1
-                textlist = textlist + "{0:25} {1} \n".format(user, timestamp[:-13])
-    textlist = textlist + "\n\n Users: {}".format(usercount)
-    return textlist
 
 
 def check_priv(message: Message, bot: DeltaBot) -> None:
@@ -162,3 +145,28 @@ def check_priv(message: Message, bot: DeltaBot) -> None:
     dbot.logger.error("recieved message from not registered group or 1on1.")
     dbot.logger.error("Sender: {} Chat: {}".format(message.get_sender_contact().addr, message.chat.get_name()))
     return False
+
+
+def writetofile(sign, startdate, now):
+    usercount = 0
+    path = os.path.join(os.path.dirname(dbot.account.db_path), __name__)
+    filename = os.path.join(path, 'summary-{}.txt'.format(now))
+    dbot.logger.info("saving file to: {}".format(filename))
+    with open(filename, 'w') as file:
+        if sign == 1: 
+            file.write("Users who have been seen since {}".format(startdate))
+        if sign == 0: 
+            file.write("Users who have NOT been seen since {}".format(startdate))
+        for user, timestamp in db.list_mailusers():
+            if sign == 2:
+                usercount = usercount + 1
+                file.writelines("{0:25} {1} \n".format(user, timestamp[:-13]))
+            if sign == 1 and startdate < datetime.fromisoformat(timestamp):
+                usercount = usercount + 1
+                file.writelines("{0:25} {1} \n".format(user, timestamp[:-13]))
+            if sign == 0 and startdate > datetime.fromisoformat(timestamp):
+                usercount = usercount + 1
+                file.writelines("{0:25} {1} \n".format(user, timestamp[:-13]))
+    return usercount, filename
+
+
